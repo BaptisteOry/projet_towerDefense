66793f8f192d1325de81c17177271b843e4c7326
Solène <45456425+SolHaine@users.noreply.github.com>
2019-05-12 21:37:54 +0200

    Debug

--- src/main.c
+++ src/main.c
@@ -15,15 +15,52 @@ static const unsigned int WINDOW_WIDTH = 600;
 static const unsigned int WINDOW_HEIGHT = 600;
 static const char WINDOW_TITLE[] = "Tower Defense";
 
+static float aspectRatio;
+
 /* Espace fenetre virtuelle */
 static const float GL_VIEW_WIDTH = 150.;
 static const float GL_VIEW_HEIGHT = 150.;
+static const float GL_VIEW_SIZE = 150.;
 
 /* Nombre de bits par pixel de la fenetre */
 static const unsigned int BIT_PER_PIXEL = 32;
 
 /* Nombre minimal de millisecondes separant le rendu de deux images */
 static const Uint32 FRAMERATE_MILLISECONDS = 1000 / 60;
+
+
+void reshape(SDL_Surface** surface, unsigned int width, unsigned int height)
+{ 
+    SDL_Surface* surface_temp = SDL_SetVideoMode(   
+        width, height, BIT_PER_PIXEL,
+        SDL_OPENGL | SDL_GL_DOUBLEBUFFER | SDL_RESIZABLE);
+    if(NULL == surface_temp) 
+    {
+        fprintf(
+            stderr, 
+            "Erreur lors du redimensionnement de la fenetre.\n");
+        exit(EXIT_FAILURE);
+    }
+    *surface = surface_temp;
+
+    aspectRatio = width / (float) height;
+
+    glViewport(0, 0, (*surface)->w, (*surface)->h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    if( aspectRatio > 1) 
+    {
+        gluOrtho2D(
+        -GL_VIEW_SIZE / 2. * aspectRatio, GL_VIEW_SIZE / 2. * aspectRatio, 
+        -GL_VIEW_SIZE / 2., GL_VIEW_SIZE / 2.);
+    }
+    else
+    {
+        gluOrtho2D(
+        -GL_VIEW_SIZE / 2., GL_VIEW_SIZE / 2.,
+        -GL_VIEW_SIZE / 2. / aspectRatio, GL_VIEW_SIZE / 2. / aspectRatio);
+    }
+}
 
 int main(int argc, char** argv) 
 {
