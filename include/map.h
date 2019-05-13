8a646af0e24db49826f57fcf2abadbd50e1a24a5
Solène <45456425+SolHaine@users.noreply.github.com>
2019-05-13 22:45:35 +0200

    Vérifications partie 1

--- include/map.h
+++ include/map.h
@@ -1,28 +1,6 @@
 #ifndef MAPVERIF_H__
 #define MAPVERIF_H__
 
-typedef struct Node {
-	int id;
-	int x, y;
-	int type;
-	Node* nextNode;	
-} Node;
-
-typedef struct Link {
-	int nodeId1, nodeId2;
-	Link* nextLink;
-} Link;
-
-typedef struct Graph {
-	Node* nodes;
-	Link* links;
-};
-
-typedef struct ItdEltsInfos {
-	char keyword[10];
-	int r, g, b;
-} ItdEltsInfos;
-
 // Verifies if the .idt file is correct
 void itdCheck(char* itdFile);
 
--- src/map.c
+++ src/map.c
@@ -4,18 +4,8 @@
 #include "../include/mapVerif.h"
 
 void itdCheck(char* itdFile) {
-
-	// ITD file elements
-	char code[5];
-	int version;
-	char comments[50];
-	char keyword[10];
-	int valueR, valueG, valueB;
-	char ppmFileName[10];
-	int nodesNumber;
-
-	// Open .itd file
 	FILE *file;
+
 	file = fopen(itdFile, "r"); // read mode
  
 	if (file == NULL) {
@@ -23,49 +13,16 @@ void itdCheck(char* itdFile) {
 	  	return EXIT_FAILURE;
 	}
 
-	/* FILE CHECK */
-
-	// itd code check
-	fscanf(file, "%s %d", code, &version);
-	printf("%s\n", code);
-	if(code != "@ITD") {
-		printf("ERROR : Wrong .itd file format.\n");
-		EXIT_FAILURE;
-	}
-	if(version != 1) {
-		printf("Itd version not supported.\n");
-		EXIT_FAILURE;
-	}
-
-	// Read comment
-	fgets(comments, sizeof comments, file);
-	if(comments[0] != #) {
-		printf("Second line isn't comments.")
-		EXIT_FAILURE
-	}
-	fputs(comments, stdout);
-
-	// Read variables & values
-
-	// Store variables & values
-
-	// Read nodes description
-
-	// Store nodes descrition by creating the graph
-
-
-	/* PPM MAP CHECK */
-
-
+/*	char data; 
+	fscanf(file, "%s", data&);
+	printf("%s\n", data);
+	if(data != "@ITD 1") {
+		printf("ERROR : Wrong .itd file format.\n", );
+	}*/
  
- 	// Close .itd file
 	fclose(file);
 }
 
-void mapCheck(ImageMap map, char* itdFile, ItsEltsInfos* infos) {
+void mapCheck(ImageMap map, char* itdFile) {
 	
 }
-
-/*void createGraph(Graph* graph) {
-
-}*/
